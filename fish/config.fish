eb296e98ac62794b71ad30821dd1aac8d4e1fa56
NatumeMituan <NatumeMituan@gmail.com>
2023-08-17 15:50:36 +0800

    Add catppuccin theme for fzf and bat

--- fish/config.fish
+++ fish/config.fish
@@ -4,7 +4,11 @@ setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
 setenv FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
 setenv FZF_ALT_C_COMMAND $FZF_CTRL_T_COMMAND
 
-setenv FZF_DEFAULT_OPTS '--height 8% --layout=reverse --border'
+setenv FZF_DEFAULT_OPTS '
+--height 80% --layout=reverse --border
+--color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
+--color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+--color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796'
 
 fish_vi_key_bindings
 
