9aef5bbdebbaf7a039aaa492bcf012fbaa0fc853
NatumeMituan <NatumeMituan@gmail.com>
2023-08-25 20:13:23 +0800

    Fishshell: add abbr for diff

--- fish/config.fish
+++ fish/config.fish
@@ -6,11 +6,12 @@ setenv FZF_DEFAULT_COMMAND 'fd --type file --follow'
 setenv FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND --search-path \$dir"
 setenv FZF_ALT_C_COMMAND $FZF_CTRL_T_COMMAND
 
+# https://github.com/catppuccin/fzf
 setenv FZF_DEFAULT_OPTS '
---height 80% --layout=reverse --border
---color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
---color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
---color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796'
+    --height 80% --layout=reverse --border
+    --color=bg+:#363a4f,bg:#24273a,spinner:#f4dbd6,hl:#ed8796
+    --color=fg:#cad3f5,header:#ed8796,info:#c6a0f6,pointer:#f4dbd6
+    --color=marker:#f4dbd6,fg+:#cad3f5,prompt:#c6a0f6,hl+:#ed8796'
 
 fish_vi_key_bindings
 
@@ -19,6 +20,9 @@ abbr -a e nvim
 abbr -a g git
 abbr -a gst 'git status'
 abbr -a glo 'git log --oneline --decorate'
+abbr -a gd 'git diff'
+abbr -a gdd 'git difftool'
+abbr -a nd 'nvim -d'
 
 # https://github.com/ogham/exa
 if type -p exa > /dev/null
