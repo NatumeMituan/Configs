724ba37a16c0960ded091f9be997074988b7e437
NatumeMituan <NatumeMituan@gmail.com>
2023-09-10 00:36:44 +0800

    [nvim] Utilize indent-blankline

--- nvim/init.lua
+++ nvim/init.lua
@@ -138,6 +138,9 @@ require('lazy').setup({
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
+    config = function()
+      require("colorscheme")
+    end,
   },
 
   {
@@ -163,8 +166,8 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     opts = {
-      char = '┊',
       show_trailing_blankline_indent = false,
+      show_current_context = true,
     },
   },
 
@@ -257,6 +260,10 @@ vim.o.expandtab = true
 -- Cursor line
 vim.o.cursorline = true
 
+vim.opt.list = true
+vim.opt.listchars:append "space:⋅"
+vim.opt.listchars:append "eol:↴"
+
 -- Set highlight on search
 vim.o.hlsearch = false
 
@@ -334,63 +341,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   pattern = '*',
 })
 
--- [[ Configure catppuccin]]
-require("catppuccin").setup({
-  flavour = "macchiato", -- latte, frappe, macchiato, mocha
-  background = {         -- :h background
-    light = "latte",
-    dark = "macchiato",
-  },
-  transparent_background = false, -- disables setting the background color.
-  show_end_of_buffer = false,     -- shows the '~' characters after the end of buffers
-  term_colors = false,            -- sets terminal colors (e.g. `g:terminal_color_0`)
-  dim_inactive = {
-    enabled = false,              -- dims the background color of inactive window
-    shade = "dark",
-    percentage = 0.15,            -- percentage of the shade to apply to the inactive window
-  },
-  no_italic = false,              -- Force no italic
-  no_bold = false,                -- Force no bold
-  no_underline = false,           -- Force no underline
-  styles = {                      -- Handles the styles of general hi groups (see `:h highlight-args`):
-    comments = { "italic" },      -- Change the style of comments
-    conditionals = { "italic" },
-    loops = {},
-    functions = {},
-    keywords = {},
-    strings = {},
-    variables = {},
-    numbers = {},
-    booleans = {},
-    properties = {},
-    types = {},
-    operators = {},
-  },
-  color_overrides = {},
-  custom_highlights = function(cp)
-    return {
-      LineNr = { fg = cp.text, bg = cp.surface0 },
-      CursorLineNr = { fg = cp.peach },
-    }
-  end,
-  integrations = {
-    cmp = true,
-    gitsigns = true,
-    nvimtree = true,
-    treesitter = true,
-    which_key = true,
-    telescope = {
-      enabled = true,
-      style = "nvchad"
-    },
-    notify = false,
-    mini = false,
-    -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-  },
-})
-
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
