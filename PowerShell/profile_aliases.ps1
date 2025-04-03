204f31ef4db68806caacbb87e11cfc52eee8769a
NatumeMituan <NatumeMituan@gmail.com>
2025-04-03 22:46:22 +0800

    [PowerShell] Split and modularize the profile script

---
+++ PowerShell/profile_aliases.ps1
@@ -0,0 +1,11 @@
+# https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/using-aliases
+
+# PowerShell
+Set-Alias which Get-Command
+Set-Alias new New-Item
+
+# Editor
+Set-Alias -Name e code
+
+# Scoop
+Set-Alias -Name scp scoop
---
+++ PowerShell/profile_settings.ps1
@@ -0,0 +1,19 @@
+# PSReadLine
+# https://github.com/PowerShell/PowerShell
+Set-PSReadLineOption -PredictionViewStyle ListView
+
+# oh-my-posh
+# https://github.com/jandedobbeleer/oh-my-posh
+# https://ohmyposh.dev/docs/installation/prompt
+oh-my-posh init pwsh --config "$env:POSH_THEMES_PATH\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+
+# zoxide
+# https://github.com/ajeetdsouza/zoxide
+# scoop install main/zoxide
+Invoke-Expression (& { (zoxide init powershell | Out-String) })
+
+# CompletionPredictor
+# https://github.com/PowerShell/CompletionPredictor
+# https://learn.microsoft.com/en-us/powershell/scripting/learn/shell/using-predictors
+# Install-Module -Name CompletionPredictor -Repository PSGallery
+Import-Module CompletionPredictor
