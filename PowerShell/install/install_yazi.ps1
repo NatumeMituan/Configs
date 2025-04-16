2b894ed7d5d4646fc549a15f9286bdc029a7653f
NatumeMituan <NatumeMituan@gmail.com>
2025-04-17 07:51:50 +0800

    [yazi] Add smart-enter plugin and some keymaps

--- PowerShell/install/install_yazi.ps1
+++ PowerShell/install/install_yazi.ps1
@@ -11,3 +11,7 @@ scoop install main/yazi
 # https://github.com/yazi-rs/flavors/tree/main/catppuccin-latte.yazi
 ya pack -a yazi-rs/flavors:catppuccin-macchiato
 ya pack -a yazi-rs/flavors:catppuccin-latte
+
+# smart-enter plugin
+# https://github.com/yazi-rs/plugins/tree/main/smart-enter.yazi
+ya pack -a yazi-rs/plugins:smart-enter
