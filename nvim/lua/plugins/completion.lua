7c9593b962d3290a22c7e7c7a13acd1642606522
NatumeMituan <NatumeMituan@gmail.com>
2025-04-18 00:39:52 +0800

    [nvim] Delete <CR> for nvim-cmp

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -23,7 +23,6 @@ return {
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
                 ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
