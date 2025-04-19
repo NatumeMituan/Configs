93c96152aa179ed6742f41e44cd4c50deab72d0d
NatumeMituan <NatumeMituan@gmail.com>
2025-04-19 20:58:23 +0800

    [nvim] Config colorscheme Catppuccin

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -4,13 +4,32 @@ return {
     name = "catppuccin",
     priority = 1000,
     config = function()
+        -- Reference:
+        --   - https://www.lazyvim.org/plugins/colorscheme#catppuccin
         require("catppuccin").setup({
             flavour = "macchiato",
+            transparent_background = true,
+            custom_highlights = function(cp)
+                return {
+                    LineNr = { fg = cp.text, bg = cp.surface0 },
+                    CursorLineNr = { fg = cp.peach },
+                }
+            end,
             integrations = {
+                mason = true,
+                native_lsp = {
+                    underlines = {
+                        errors = { "undercurl" },
+                        hints = { "undercurl" },
+                        warnings = { "undercurl" },
+                        information = { "undercurl" },
+                    },
+                },
                 telescope = {
                     enabled = true,
                     style = "nvchad"
-                }
+                },
+                which_key = true
             }
         })
         vim.cmd.colorscheme "catppuccin"
