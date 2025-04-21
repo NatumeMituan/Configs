3aebe2efb90a339d19c56b926858c756d71b8db6
NatumeMituan <NatumeMituan@gmail.com>
2025-04-21 20:05:41 +0800

    [nvim] Add lazy.nvim plugin update status to lualine

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -2,13 +2,25 @@ return {
     -- https://github.com/nvim-lualine/lualine.nvim
     'nvim-lualine/lualine.nvim',
     event = 'VeryLazy',
-    opts = {
-        options = {
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    opts = function(_, _)
+        local lualine = require('lualine')
+        local opts = lualine.get_config()
+
+        opts.options = {
             icons_enabled = true,
             theme = 'catppuccin',
             component_separators = '|',
-            section_separators = ''
+            section_separators = '',
         }
-    },
-    dependencies = { { 'nvim-tree/nvim-web-devicons' } }
+
+        -- https://lazy.folke.io/usage
+        table.insert(opts.sections.lualine_x, 1, {
+            require("lazy.status").updates,
+            cond = require("lazy.status").has_updates,
+            color = { fg = require("catppuccin.palettes").get_palette('macchiato').peach },
+        })
+
+        return opts
+    end,
 }
