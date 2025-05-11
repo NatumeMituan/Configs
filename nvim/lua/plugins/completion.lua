33ae8d2fcd4c759f1312ebb06d1c5e5596462079
NatumeMituan <NatumeMituan@gmail.com>
2025-05-11 13:18:30 +0800

    [nvim] Remove select=true for <CR> in nvim-cmp to insert newline when nothing selected explicitly

--- nvim/lua/plugins/completion.lua
+++ nvim/lua/plugins/completion.lua
@@ -22,8 +22,7 @@ return {
                 ['<C-u>'] = cmp.mapping.scroll_docs(-4),
                 ['<C-d>'] = cmp.mapping.scroll_docs(4),
                 ['<C-Space>'] = cmp.mapping.complete(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
-                ['<C-c>'] = cmp.mapping.confirm({ select = true }),
+                ['<CR>'] = cmp.mapping.confirm(),
                 ['<C-e>'] = cmp.mapping.abort(),
                 ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
