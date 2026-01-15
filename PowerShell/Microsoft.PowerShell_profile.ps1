79805185280256eb60b331e33f2480ce1c2c3fa3
NatumeMituan <NatumeMituan@gmail.com>
2026-01-15 14:36:24 +0800

    pwsh: Load profiles only for interactive sessions

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,6 @@
-# Dot source additional profile scripts
-. (Join-Path $PSScriptRoot "functions.ps1")
-. (Join-Path $PSScriptRoot "aliases.ps1")
-. (Join-Path $PSScriptRoot "settings.ps1")
+if ($Host.UI.RawUI) {
+    # Dot source additional profile scripts
+    . (Join-Path $PSScriptRoot "functions.ps1")
+    . (Join-Path $PSScriptRoot "aliases.ps1")
+    . (Join-Path $PSScriptRoot "settings.ps1")
+}
